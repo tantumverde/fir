fe37f32bf6c6fe3bc62ac2e5b3c5354ce4dd7e98
tantumverde <48627288+tantumverde@users.noreply.github.com>
2025-01-15 08:49:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.mobile-menu-button');
+    const navMenu = document.querySelector('.nav-menu');
+    menuButton.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        // Анимация кнопки бургер-меню
+        this.classList.toggle('active');
+    });
+});
+// Добавляем обработчик для кнопки прокрутки
+document.getElementById('scrollButton').addEventListener('click', function() {
+    document.getElementById('targetParagraph').scrollIntoView({ behavior: 'smooth' });
+});
+
 document.getElementById("scrollButton").addEventListener("click", function() {
     const targetElement = document.getElementById("targetParagraph");
     targetElement.scrollIntoView({ behavior: 'smooth' });
